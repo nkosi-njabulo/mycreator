218eb1f70567dabaa900c61c3f27b1242a5628f3
Njabulo <nkosispha123@gmail.com>
2024-10-19 23:36:16 +0200

    skeleton

---
+++ src/main/java/com/interledger/MyCreator/MyCreatorApplication.java
@@ -0,0 +1,13 @@
+package com.interledger.MyCreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCreatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCreatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interledger/MyCreator/MyCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interledger.MyCreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
